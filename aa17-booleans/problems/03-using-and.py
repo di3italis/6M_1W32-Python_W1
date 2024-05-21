88ea270e3b7be81008e73d44fc59b88e115388ef
takashi <takashi.sh@proton.me>
2024-05-21 16:00:50 -0700

    python and Mod6 week 1

---
+++ aa17-booleans/problems/03-using-and.py
@@ -0,0 +1,12 @@
+# Write a function using the `and` operator that returns the Boolean result of
+# the `and` operator being applied to the function's arguments.
+
+# Write your function here.
+def And(a, b):
+    if a and b return true
+
+
+print(And(True, False))    #> False
+print(And(True, True))     #> True
+print(And(False, True))    #> False
+print(And(False, False))   #> False
